6c4ef5542c69006990c161c144fc7b1f389b6688
Yasashari <yasashari5@gmail.com>
2025-01-05 08:18:01 -0800

    day1

--- main.go
+++ main.go
@@ -4,4 +4,33 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	var myString string
+	fmt.Println(myString)
+
+	myString = "Hello World!"
+
+	var myInt int
+
+	fmt.Println(myInt)
+
+	myInt = 42
+
+	fmt.Println(myInt)
+
+	var myBool bool
+	fmt.Println(myBool)
+
+	myBool = true
+	fmt.Println(myBool)
+
+	var myByte byte = 'A'
+	fmt.Println(myByte)
+
+	var myRune rune = 'B'
+	fmt.Println(myRune)
+
+	var myComplex complex128
+	myComplex = 1 + 2i
+	fmt.Println(myComplex)
 }
