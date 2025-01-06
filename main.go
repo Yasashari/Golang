74a33132d91e60b9e17230f9f6e5c8df4fec00e2
Yasashari <yasashari5@gmail.com>
2025-01-05 23:12:51 -0800

    Bit shifting and Bit masking

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Hello World!")
@@ -33,4 +36,34 @@ func main() {
 	var myComplex complex128
 	myComplex = 1 + 2i
 	fmt.Println(myComplex)
+
+	firstname := "John"
+	Lastname := "Doe"
+	fmt.Printf("%s %s\n", firstname, Lastname)
+
+	//Right shift Operation
+	var a uint8 = 20
+	fmt.Println(strconv.FormatUint(uint64(a), 2))
+
+	result := a >> 2 // Remove the last to bits -> 101 or 5
+	fmt.Println(strconv.FormatUint(uint64(result), 2))
+	fmt.Println(result)
+
+	//Left shift Operation
+	a = a << 2 // Append two zero bits -> 10100
+	fmt.Println(strconv.FormatUint(uint64(a), 2))
+
+	//Set a bit given position
+
+	a = 5 | (1 << 1) // 101 -> 111
+	fmt.Println(strconv.FormatUint(uint64(a), 2))
+
+	// Clear a bit given position
+	a = a &^ (1 << 1) // 111 -> 101
+	fmt.Println(strconv.FormatUint(uint64(a), 2))
+
+	// Toggle a bit given position
+	a = a ^ (1 << 0) // 101 -> 100
+	fmt.Println(strconv.FormatUint(uint64(a), 2))
+
 }
