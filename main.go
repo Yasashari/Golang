f88b795aeaf370a2e61c0ec14f022e59ae3d0687
Yasashari <yasashari5@gmail.com>
2025-01-08 10:14:26 -0800

    40 lessons completed

--- main.go
+++ main.go
@@ -1,69 +1,213 @@
+// package main
+
+// import "fmt"
+
+// // A funciton that returns a function
+// func createAdder(x int) func(int) int {
+// 	return func(y int) int {
+// 		return x + y
+// 	}
+// }
+
+// func main() {
+// 	addFive := createAdder(5)
+// 	fmt.Println(addFive(10))
+// }
+
+// package main
+
+// import "fmt"
+
+// func double(x int) int {
+// 	return x * 2
+// }
+
+// func triple(x int) int {
+// 	return x * 3
+// }
+
+// func apply(f func(int) int, x int) int {
+// 	return f(x)
+// }
+
+// func main() {
+// 	result := apply(double, 5)
+// 	fmt.Println(result)
+
+// 	resultTriple := apply(triple, 5)
+// 	fmt.Println(resultTriple)
+
+// 	fiveTime := func(x int) int {
+// 		return x * 5
+// 	}
+
+// 	resultFiveTime := apply(fiveTime, 5)
+// 	fmt.Println(resultFiveTime)
+// }
+
+// package main
+
+// import "fmt"
+
+// type multiplier func(int) int
+
+// func multiplyBy(m int) multiplier {
+// 	return func(i int) int {
+// 		return i * m
+// 	}
+// }
+
+// func main() {
+// 	multiplyByTwo := multiplyBy(2)
+// 	mutlyByThree := multiplyBy(3)
+
+// 	result1 := multiplyByTwo(5)
+// 	result2 := mutlyByThree(3)
+
+// 	fmt.Println(result1, result2)
+// }
+
+// package main
+
+// import "fmt"
+
+// type operration func(int, int) int
+
+// func arithmeticOperation(op string) operration {
+// 	switch op {
+// 	case "add":
+// 		return func(i1, i2 int) int {
+// 			return i1 + i2
+// 		}
+
+// 	case "subtract":
+// 		return func(i1, i2 int) int {
+// 			return i1 - i2
+// 		}
+
+// 	default:
+// 		return func(i1, i2 int) int {
+// 			return i1 * i2
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	var perform operration
+// 	perform = arithmeticOperation("add")
+// 	result := perform(2, 3)
+// 	fmt.Println(result)
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	numbers := []int{1, 2, 3, 4, 5}
+
+// 	squared := sliceOperation(numbers, func(i int) int {
+// 		return i * i
+
+// 	})
+
+// 	fmt.Println(squared)
+// }
+
+// func sliceOperation(s []int, op func(int) int) []int {
+// 	result := make([]int, len(s))
+// 	for i, v := range s {
+// 		result[i] = op(v)
+// 	}
+// 	return result
+// }
+
+//closures
+// package main
+
+// import "fmt"
+
+// func counter() func() int {
+// 	count := 0
+
+// 	return func() int {
+// 		count++
+// 		return count
+// 	}
+// }
+
+// func main() {
+// 	increment := counter()
+// 	fmt.Println(increment())
+// 	fmt.Println(increment())
+// }
+
+//anonymous functions
+
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	func() {
+// 		fmt.Println("Anonymous function")
+// 	}()
+
+// 	func(msg string) {
+// 		fmt.Println("Message", msg)
+// 	}("Hello World")
+// }
+
+//defer funcitons
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+
+// 	message := "Hello,"
+// 	greetingFn := func(name string) {
+// 		fmt.Println(message, name)
+
+// 	}
+
+// 	defer greetingFn("Alice")
+// 	defer greetingFn("Bob")
+
+// 	message = "Hi,"
+// }
+
+// Pointers
+
+// var pointerToInt *int
+// var pointerToString *string
+// var pointerToBool *bool
+
+// age := 10
+//pointerToInt = &age
+
+//ageValue := *pointerToInt
+
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
 
-	var myString string
-	fmt.Println(myString)
+	var intPtr *int
 
-	myString = "Hello World!"
+	age := 10
+	intPtr = &age
+	fmt.Println(intPtr)
+	fmt.Println(*intPtr)
 
-	var myInt int
-
-	fmt.Println(myInt)
-
-	myInt = 42
-
-	fmt.Println(myInt)
-
-	var myBool bool
-	fmt.Println(myBool)
-
-	myBool = true
-	fmt.Println(myBool)
-
-	var myByte byte = 'A'
-	fmt.Println(myByte)
-
-	var myRune rune = 'B'
-	fmt.Println(myRune)
-
-	var myComplex complex128
-	myComplex = 1 + 2i
-	fmt.Println(myComplex)
-
-	firstname := "John"
-	Lastname := "Doe"
-	fmt.Printf("%s %s\n", firstname, Lastname)
-
-	//Right shift Operation
-	var a uint8 = 20
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
-	result := a >> 2 // Remove the last to bits -> 101 or 5
-	fmt.Println(strconv.FormatUint(uint64(result), 2))
-	fmt.Println(result)
-
-	//Left shift Operation
-	a = a << 2 // Append two zero bits -> 10100
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
-	//Set a bit given position
-
-	a = 5 | (1 << 1) // 101 -> 111
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
-	// Clear a bit given position
-	a = a &^ (1 << 1) // 111 -> 101
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
-	// Toggle a bit given position
-	a = a ^ (1 << 0) // 101 -> 100
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
+	name := "Yasas"
+	namePtr := &name
+	fmt.Println(namePtr)
+	fmt.Println(*namePtr)
 
 }
