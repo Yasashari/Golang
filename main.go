a539955fa6abf33c64498e70f448b9f3498b3fab
Yasashari <yasashari5@gmail.com>
2025-01-11 07:52:22 -0800

    Completed 50 videos

--- main.go
+++ main.go
@@ -1,69 +1,397 @@
+// package main
+
+// import (
+// 	"errors"
+// 	"fmt"
+// )
+
+// type student struct {
+// 	firstName string
+// 	lastName  string
+// }
+
+// func main() {
+// 	// a := 10
+// 	// increment(&a)
+// 	// fmt.Println(a)
+
+// 	s := student{
+// 		firstName: "John",
+// 		lastName:  "Smith",
+// 	}
+
+// 	previousLastName, err := updateLastName(&s, "")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(previousLastName)
+
+// }
+
+// // func increment(x *int) {
+// // 	*x++
+// // }
+
+// func updateLastName(s *student, newLastName string) (*string, error) {
+
+// 	if newLastName == "" {
+// 		return nil, errors.New("empty new last name")
+// 	}
+
+// 	previous := s.lastName
+// 	s.lastName = newLastName
+// 	return &previous, nil
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+//func main() {
+
+// s := []int{1, 2}
+
+// fmt.Println(s)
+
+// somethingSlice(s)
+
+// fmt.Println(s)
+
+// 	myMap := map[int]string{
+// 		0: "one",
+// 		1: "two",
+// 	}
+
+// 	fmt.Println(myMap)
+
+// 	somethingMap(myMap)
+
+// 	fmt.Println(myMap)
+// }
+
+// func somethingSlice(s []int) {
+// 	//s[0] = 100
+// 	s = append(s, 100)
+// }
+
+// func somethingMap(m map[int]string) {
+// 	delete(m, 0)
+// }
+
+// methods
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// type Person struct {
+// 	Name string
+// 	Age  string
+// }
+
+// type Circle struct {
+// 	Radius float64
+// }
+
+// type Rectangle struct {
+// 	Width  float64
+// 	Height float64
+// }
+
+// func (c Circle) Area() float64 {
+// 	return 3.14 * c.Radius * c.Radius
+// }
+
+// func (p Person) GetDetails() string {
+// 	return fmt.Sprintf("Name: %s, Age: %s", p.Name, p.Age)
+// }
+
+// func (r *Rectangle) Area() float64 {
+// 	if r == nil {
+// 		return 0
+// 	}
+// 	return r.Width * r.Height
+// }
+
+// func (r *Rectangle) SetWidth(w float64) {
+// 	r.Width = w
+// }
+
+// func updateWidth(r *Rectangle, w float64) {
+// 	r.SetWidth(w)
+// }
+
+// func main() {
+// 	p := Person{
+// 		Name: "John",
+// 		Age:  "30",
+// 	}
+
+// 	fmt.Println(p.GetDetails())
+
+// 	rect := Rectangle{
+// 		Width:  10,
+// 		Height: 5,
+// 	}
+
+// 	fmt.Println("Area of Rectangle:", rect.Area())
+// 	rect.SetWidth(20)
+// 	fmt.Println("Area of Rectangle:", rect.Area())
+
+// 	var rect2 *Rectangle
+
+// 	fmt.Println("Area of Rectangle2:", rect2.Area())
+
+// 	updateWidth(&rect, 20)
+
+// 	fmt.Println("Area of Rectangle3:", rect.Area())
+// }
+
+// package main
+
+// import "fmt"
+
+// type Calculator struct {
+// }
+
+// func (c *Calculator) Add(a, b int) int {
+// 	return a + b
+// }
+
+// func AddFunciton(a, b int) int {
+// 	return a + b
+// }
+
+// func ArithmeticOperation(f func(int, int) int, a, b int) int {
+// 	return f(a, b)
+// }
+
+// func main() {
+// 	fmt.Println(AddFunciton(10, 15))
+// 	fmt.Println(ArithmeticOperation(AddFunciton, 10, 15))
+// 	c := Calculator{}
+// 	fmt.Println(ArithmeticOperation(c.Add, 10, 15))
+// }
+
+// package main
+
+// import "fmt"
+
+// type Printer struct{}
+
+// func (p *Printer) Print(m string) {
+// 	fmt.Println(m)
+// }
+
+// func main() {
+// 	printer := &Printer{}
+
+// 	printFunction := printer.Print
+
+// 	printFunction("Hello World!")
+// }
+
+// package main
+
+// import "fmt"
+
+// type Person struct {
+// 	Name string
+// 	Age  int
+// }
+
+// func (p Person) GetDetails() string {
+// 	return fmt.Sprintf("Name: %s, Age: %d", p.Name, p.Age)
+// }
+
+// func main() {
+// 	p := Person{
+// 		Name: "John",
+// 		Age:  30,
+// 	}
+
+// 	f1 := Person.GetDetails
+// 	fmt.Println(f1(p))
+
+// }
+
+//package main
+
+//import "fmt"
+
+//type Age int
+
+// type Human struct {
+// 	Age int
+// }
+
+//type Student Human
+
+// func sum(a Age) {
+// 	fmt.Printf("Age: %d\n", a)
+// }
+
+// func (h Human) printAge() {
+// 	fmt.Println(h.Age)
+// }
+
+//type Student Human
+
+//func main() {
+// var young Age = 10
+// var old Age = 100
+// fmt.Println(young + old)
+// sum(young)
+
+//var s = Student{Age: 10}
+//fmt.Println(s.Age)
+//var newYoung int = 10
+
+//sum(newYoung)
+//}
+
+// iota
+
+// package main
+
+// import "fmt"
+
+// type Size int
+
+// const (
+// 	ExtraSmall Size = iota
+// 	Small
+// 	Medium
+// 	Large
+// 	ExtraLarge
+// )
+
+// func main() {
+// 	fmt.Println(ExtraSmall)
+// 	fmt.Println(Small)
+// 	fmt.Println(Medium)
+// 	fmt.Println(Large)
+// 	fmt.Println(ExtraLarge)
+// }
+
+// package main
+
+// import "fmt"
+
+// type Engine struct {
+// 	HorsePower int
+// }
+
+// type GPS struct {
+// 	Model string
+// }
+
+// type Car struct {
+// 	Model string
+// 	Engine
+// 	GPS
+// }
+
+// func (e *Engine) Start() {
+// 	fmt.Println("Engine started")
+// }
+
+// func (c *Car) Drive() {
+// 	fmt.Printf("Driving my %s...\n", c.Model)
+// }
+
+// type Car struct {
+// 	Model string
+// 	Engine
+// }
+
+//func main() {
+// myCar := Car{
+// 	Model: "Mustang",
+// 	Engine: Engine{
+// 		HorsePower: 300,
+// 	},
+// }
+// fmt.Println(myCar.Model, myCar.HorsePower)
+
+// myCar.Start()
+// myCar.Drive()
+
+// 	myCar := Car{
+// 		Model: "Mustang",
+// 		Engine: Engine{
+// 			HorsePower: 300,
+// 		},
+// 		GPS: GPS{
+// 			Model: "Garmin",
+// 		},
+// 	}
+
+// 	fmt.Println(myCar.Model, myCar.HorsePower, myCar.GPS.Model)
+// }
+
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
+type Shape interface {
+	Area() float64
+}
+
+type Object interface {
+	Name() string
+	Shape
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Name() string {
+	return "Rectangle"
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+func printArea(s Shape) {
+	fmt.Println(s.Area())
+}
+
+func printObject(o Object) {
+	fmt.Printf("Area:%f , Name: %s\n", o.Area(), o.Name())
+}
 func main() {
-	fmt.Println("Hello World!")
+	r := Rectangle{
+		Width:  10,
+		Height: 5,
+	}
 
-	var myString string
-	fmt.Println(myString)
+	c := Circle{
+		Radius: 5,
+	}
 
-	myString = "Hello World!"
+	shapes := []Shape{r, c}
 
-	var myInt int
+	for _, shape := range shapes {
+		//fmt.Printf("%f\n", shape.Area())
+		printArea(shape)
 
-	fmt.Println(myInt)
+	}
 
-	myInt = 42
-
-	fmt.Println(myInt)
-
-	var myBool bool
-	fmt.Println(myBool)
-
-	myBool = true
-	fmt.Println(myBool)
-
-	var myByte byte = 'A'
-	fmt.Println(myByte)
-
-	var myRune rune = 'B'
-	fmt.Println(myRune)
-
-	var myComplex complex128
-	myComplex = 1 + 2i
-	fmt.Println(myComplex)
-
-	firstname := "John"
-	Lastname := "Doe"
-	fmt.Printf("%s %s\n", firstname, Lastname)
-
-	//Right shift Operation
-	var a uint8 = 20
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
-	result := a >> 2 // Remove the last to bits -> 101 or 5
-	fmt.Println(strconv.FormatUint(uint64(result), 2))
-	fmt.Println(result)
-
-	//Left shift Operation
-	a = a << 2 // Append two zero bits -> 10100
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
-	//Set a bit given position
-
-	a = 5 | (1 << 1) // 101 -> 111
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
-	// Clear a bit given position
-	a = a &^ (1 << 1) // 111 -> 101
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
-	// Toggle a bit given position
-	a = a ^ (1 << 0) // 101 -> 100
-	fmt.Println(strconv.FormatUint(uint64(a), 2))
-
+	printObject(r)
 }
